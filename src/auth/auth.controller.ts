5973bc2d465bd8233036de9bff0e6ae5ff62fb32
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-11-18 20:17:08 -0600

    useGuard Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Headers, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser, RawHeader } from './decorators';
 import { User } from './entities';
 import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role.guard';
 
 
 @Controller('auth')
@@ -31,6 +32,8 @@ export class AuthController {
    @RawHeader('rawHeaders') rawHeaders: string[],
    @Headers() headers: IncomingHttpHeaders
   ){
+
+
     
     return {
       ok: true,
@@ -38,6 +41,18 @@ export class AuthController {
       user,
       userEmail,
       rawHeaders
+    }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser() user: User,
+  ){
+    return {
+      ok: true,
+      user
     }
   }
 
--- src/auth/decorators/raw-headers.decorator.ts
+++ src/auth/decorators/raw-headers.decorator.ts
@@ -6,10 +6,13 @@ import {
 export const RawHeader = createParamDecorator(
     (data, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
+        const headers = request.rawHeaders;
         
 
         if(!request)throw new InternalServerErrorException('Request not found');
-            
+        //if(!headers)throw new InternalServerErrorException('Headers not found');
+        //return headers;
+
         return (!data) ? 'No RawHeader Provided' : request[data];
         
     }
